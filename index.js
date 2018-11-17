2bfb97735e219e82c1462600657bee5f3f80072e
dekc89 <dekc89@gmail.com>
2018-11-17 13:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // add solution here
 
-// const theBeatlesPlay = (arrMus, arrInst) => {
-//   let finalArray = [];
-//   for (let i = 0; i < arrMus.legth; i++) {
-//     finalArray.push(`${arrMus[i]} plays ${arrInst[i]}`)
-//   }
-//   return finalArray;
-// }
-
-function theBeatlesPlay(arrMus, arrInst) {
-  var finalArray = [];
-  for (let i = 0; i < arrMus.length; i++) {
-    finalArray.push(arrMus[i] + " plays " + arrInst[i]);
+const theBeatlesPlay = (arrMus, arrInst) => {
+  let finalArray = [];
+  for (let i = 0; i < arrMus.legth; i++) {
+    finalArray.push(`${arrMus[i]} plays ${arrInst[i]}`)
   }
   return finalArray;
 }
+
+// function theBeatlesPlay(arrMus, arrInst) {
+//   var finalArray = [];
+//   for (let i = 0; i < arrMus.length; i++) {
+//     finalArray.push(arrMus[i] + " plays " + arrInst[i]);
+//   }
+//   return finalArray;
+// }
