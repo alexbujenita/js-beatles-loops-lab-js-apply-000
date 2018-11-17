4952a390774a325640bd48e9c375205001407289
dekc89 <dekc89@gmail.com>
2018-11-17 13:44:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+// function theBeatlesPlay(arrMus, arrInst)
+
+const theBeatlesPlay = (arrMus, arrInst) => {
+  let finalArray = [];
+  for (let i = 0; i < arrMus.legth; i++) {
+    finalArray.push(`${arrMus[i]} plays ${arrInst[i]}.`)
+  }
+}
